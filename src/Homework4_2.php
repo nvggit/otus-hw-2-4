8c934552f7183872249757329bfc49f4805f4d0a
nvggit <cubaman@mail.ru>
2019-03-14 13:08:27 +0300

    init

---
+++ src/Homework4_2.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Homework4_2;
+
+/**
+ * Example class for homework
+ * @package Homework4_2
+ */
+class Homework4_2
+{
+    public static function passHomeWork()
+    {
+        return true;
+    }
+}
